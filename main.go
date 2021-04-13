66704552051cf4b3d8035b70c7128b2bf0cbc34c
yuki0920 <ywsep20@gmail.com>
2021-04-13 21:12:58 +0900

    Connect to db

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
+	db, err := gorm.Open(sqlite.Open("go_admin"), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect to database")
+	}
+
+	fmt.Println(db)
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
