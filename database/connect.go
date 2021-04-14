f0fad27c03314744dfaaca7fd2bfadddd723bd01
yuki0920 <ywsep20@gmail.com>
2021-04-15 07:58:43 +0900

    Add auto migrate

--- database/connect.go
+++ database/connect.go
@@ -1,14 +1,17 @@
 package database
 
 import (
+	"github.com/yuki0920/company_board/models"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func Connect() {
-	_, err := gorm.Open(sqlite.Open("go_admin"), &gorm.Config{})
+	database, err := gorm.Open(sqlite.Open("go_admin"), &gorm.Config{})
 
 	if err != nil {
 		panic("Could not connect to database")
 	}
+
+	database.AutoMigrate(&models.User{})
 }
