05c56fab35544f63904c603b7f8e7e0bf54ca5b7
yuki0920 <ywsep20@gmail.com>
2021-04-16 07:33:18 +0900

    Store jwt to cookie

--- controllers/authController.go
+++ controllers/authController.go
@@ -76,5 +76,16 @@ func Login(c *fiber.Ctx) error {
 		return c.SendStatus(fiber.StatusInternalServerError)
 	}
 
-	return c.JSON(token)
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24), // 1day
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
 }
