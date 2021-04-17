35a697451bba431af2d7d6e52d569078f497f76b
yuki0920 <ywsep20@gmail.com>
2021-04-17 16:43:58 +0900

    Add api/roles

--- routes/routes.go
+++ routes/routes.go
@@ -15,9 +15,16 @@ func Setup(app *fiber.App) {
 
 	app.Get("api/user", controllers.User)
 	app.Post("api/logout", controllers.Logout)
+
 	app.Get("api/users", controllers.AllUsers)
 	app.Post("api/users", controllers.CreateUser)
 	app.Get("api/users/:id", controllers.GetUser)
 	app.Put("api/users/:id", controllers.UpdateUser)
 	app.Delete("api/users/:id", controllers.DeleteUser)
+
+	app.Get("api/roles", controllers.AllRoles)
+	app.Post("api/roles", controllers.CreateRole)
+	app.Get("api/roles/:id", controllers.GetRole)
+	app.Put("api/roles/:id", controllers.UpdateRole)
+	app.Delete("api/roles/:id", controllers.DeleteRole)
 }
