902440f57d0bb496e8941c87407184fa8cbf1066
yuki0920 <ywsep20@gmail.com>
2021-04-17 15:57:33 +0900

    Add post api/users

--- controllers/userController.go
+++ controllers/userController.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/yuki0920/company_board/database"
 	"github.com/yuki0920/company_board/models"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func AllUsers(c *fiber.Ctx) error {
@@ -12,4 +13,21 @@ func AllUsers(c *fiber.Ctx) error {
 	database.DB.Find(&users)
 
 	return c.JSON(users)
+}
+
+// パスワードを自動付与するテストデータ
+func CreateUser(c *fiber.Ctx) error {
+	var user models.User
+
+	if err := c.BodyParser(&user); err != nil {
+		return err
+	}
+
+	password, _ := bcrypt.GenerateFromPassword([]byte("1234"), 14)
+
+	user.Password = password
+
+	database.DB.Create(&user)
+
+	return c.JSON(user)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -16,4 +16,5 @@ func Setup(app *fiber.App) {
 	app.Get("api/user", controllers.User)
 	app.Post("api/logout", controllers.Logout)
 	app.Get("api/users", controllers.AllUsers)
+	app.Post("api/users", controllers.CreateUser)
 }
