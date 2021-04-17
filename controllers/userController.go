165344c8d85110273ae5b01ee8ceb6ca0c0a0c99
yuki0920 <ywsep20@gmail.com>
2021-04-18 07:31:56 +0900

    Add pagination

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"math"
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
@@ -9,12 +10,25 @@ import (
 )
 
 func AllUsers(c *fiber.Ctx) error {
+	page, _ := strconv.Atoi(c.Query("page", "1"))
+	limit := 5
+	offset := (page - 1) * limit
+	var total int64
+
 	var users []models.User
 
 	// Fix n+1 by preloading
-	database.DB.Preload("Role").Find(&users)
+	database.DB.Preload("Role").Offset(offset).Limit(limit).Find(&users)
+	database.DB.Model(&models.User{}).Count(&total)
 
-	return c.JSON(users)
+	return c.JSON(fiber.Map{
+		"data": users,
+		"meta": fiber.Map{
+			"total":     total,
+			"page":      1,
+			"last_page": math.Ceil(float64(int(total) / limit)),
+		},
+	})
 }
 
 // パスワードを自動付与するテストデータ
